aeb58408be19f8c61dde713642c88d0f9e8914fe
Tiago Rufino <trufino90@gmail.com>
2022-04-18 20:57:39 +0100

    Implementado a NavLink no Nabar.jsx e implementado o routing na App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Create from './componets/Create';
+import Home from './componets/Home';
+import Navbar from './componets/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          
+        </Routes>
+      </div>
+  
+    </BrowserRouter>
   );
 }
 
--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+        <ul>
+            <li>
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to="/create">Create</NavLink>
+            </li>
+        </ul>
+    </nav>
   )
 }
