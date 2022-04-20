a122eecf531a57f0161de5d26080f6bc45fef66b
Tiago Rufino <trufino90@gmail.com>
2022-04-20 20:06:53 +0100

    criação de um state para gestão do formulário de criação de stock

--- src/componets/Create.jsx
+++ src/componets/Create.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Create() {
 
+  const [formData, setFormData] = useState({
+    name: "",
+    code: "",
+    price: 0
+  });
+
   function handleChange(event) {
     console.log("handleChange: ", event.target.value);
+
+    const {name, value} = event.target;
+
+    setFormData( prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   }
   
   function handleSubmit(event) {
@@ -18,19 +31,19 @@ export default function Create() {
             <div>
               <label>
                 Nome
-                <input type="text" name="name" required minLength="4" maxLength="60" onChange={handleChange} />
+                <input type="text" name="name" required minLength="4" maxLength="60" onChange={handleChange} value={formData.name} />
               </label>
             </div>
             <div>
               <label>
                 Codigo
-                <input type="text" name="code" required minLength="3" maxLength="3"onChange={handleChange} />
+                <input type="text" name="code" required minLength="3" maxLength="3"onChange={handleChange} value={formData.code}  />
               </label>
             </div>
             <div>
               <label>
                 Preço
-                <input type="number" name="price" required min="4" max="9999" onChange={handleChange} />
+                <input type="number" name="price" required min="4" max="9999" onChange={handleChange} value={formData.price}  />
               </label>
             </div>
             <div>
